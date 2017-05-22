dc2718461f9cb2b9920126649bc5513811f2ec77
patryk.bogusz <pat049b@gmail.com>
2017-05-22 08:24:09 +0200

    Resolve conflict in app.js

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -30,7 +30,6 @@ $('.like').on('click', function(event){ // like onclick
     var dislikesElement = event.currentTarget.parentNode.parentNode.querySelector(".info").querySelector(".dislikes");
     var likes = +likesElement.innerText;
     var dislikes = +dislikesElement.innerText;
-<<<<<<< HEAD
     $.ajax({
         method:'POST',
         url: urlLike,
@@ -72,49 +71,6 @@ $('.like').on('click', function(event){ // like onclick
 
 
     });
-=======
-   $.ajax({
-        method:'POST',
-        url: urlLike,
-       data: {isLike: isLike, postId: postId, _token: token}
-   }).done(function(){
-       var clickedLike = event.target.innerText === 'You like this post' || event.target.innerText === 'Like';
-       var clickeddislike= !clickedLike;
-       var wasLiked = event.target.parentNode.querySelector(".increase").innerText === 'You like this post';
-       var wasDisliked = event.target.parentNode.querySelector(".decrease").innerText === 'You don\'t like this post';
-
-       //debugger;
-       if (clickedLike) {
-           if (wasLiked) {
-               event.target.parentNode.querySelector(".increase").innerText = 'Like';
-               likesElement.innerText = likes - 1;
-           } else if (!wasLiked) {
-               event.target.parentNode.querySelector(".increase").innerText = 'You like this post';
-               likesElement.innerText = likes + 1;
-           }
-
-           if (!wasLiked && wasDisliked) {
-               dislikesElement.innerText = dislikes - 1;
-               event.target.parentNode.querySelector(".decrease").innerText = 'Dislike';
-           }
-       } else if (clickeddislike) {
-           if (wasDisliked) {
-               event.target.parentNode.querySelector(".decrease").innerText = 'Dislike';
-               dislikesElement.innerText = dislikes - 1;
-           } else if (!wasDisliked) {
-               event.target.parentNode.querySelector(".decrease").innerText = 'You don\'t like this post';
-               dislikesElement.innerText = dislikes + 1;
-           }
-
-           if (wasLiked && !wasDisliked) {
-               likesElement.innerText = likes - 1;
-               event.target.parentNode.querySelector(".increase").innerText = 'Like';
-           }
-       }
-
-
-   });
->>>>>>> calendar
 });
 
 $('#menu-toggle').click(function (e) {
